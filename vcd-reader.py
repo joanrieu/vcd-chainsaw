5b0f3c18ace74e73349990417afb36625e1385e3
Joan Rieu <joan.rieu@gmail.com>
2015-11-20 15:21:20 +0100

    Added the VCD parser

--- vcd-reader.py
+++ vcd-reader.py
@@ -1,14 +1,39 @@
+from pprint import pprint
 import re
+import sys
 
 class VcdReader:
     
-    def __init__(self, filename):
-        file = fopen(filename, "r")
+    def __init__(self, file):
         while True:
-            line = readline()
-            if len(line) == 0:
-                break
-            line = line.strip()
-            mtime = re.search('^#(\d+)$', line)
-            mwire = re.search('^$', line) # TODO
-        fclose(file)
+            line = file.readline()
+            if not self.tryReadLine(line):
+                sys.stderr.write("unknown command: " + line);
+    
+    def tryReadLine(self, line):
+        if len(line) == 0:
+            return False
+        line = line.strip()
+        return self.tryReadTime(line) or self.tryReadWire(line)
+
+    def tryReadTime(self, line):
+        time = re.match('^#(\d+)$', line)
+        if not time:
+            return False
+        self.readTime(int(time.group(1)))
+        return True
+    
+    def readTime(self, time):
+        pass
+    
+    def tryReadWire(self, line):
+        wire = re.match('^(0|1)(\w+)$', line)
+        if not wire:
+            return False
+        name = wire.group(2)
+        value = wire.group(1)
+        print("Wire " + name + " has value " + value);
+        return True
+
+if __name__ == "__main__":
+    reader = VcdReader(sys.stdin)
