0af939b13a2229342f07159048da0f35bdd92a8e
Joan Rieu <joan.rieu@gmail.com>
2015-12-04 12:09:04 +0000

    Commented the python files

--- SerialDecoder.py
+++ SerialDecoder.py
@@ -1,12 +1,31 @@
 class SerialDecoder:
+    """
+    Decodes the bytes transmitted bit-by-bit in a serial communication and calls
+    self.onByte when a new byte is available.
+    
+    This class should be used as a parent class.
+    Call the read function with each bit read on the the wire.
+    """
     
     MSB_TO_LSB = 1
+    """Bit order: most significant first, least significant last."""
+    
     LSB_TO_MSB = 2
+    """Bit order: least significant first, most significant last."""
     
     PARITY_EVEN = 1
+    """Parity check: even number of 1s."""
+    
     PARITY_ODD = 2
+    """Parity check: odd number of 1s."""
     
     def __init__(self, dataBits, bitOrder, parityBit, stopBits):
+        """
+        dataBits(int): number of interesting bits of data transmitted
+        bitOrder(enum): order in which the data bits are transmitted
+        parityBit(enum): type of parity check or None if no parity check
+        stopBits(int): number of stop bits transmitted
+        """
         # Start
         self.state = 0
         self.states = [self.readStart]
@@ -33,32 +52,44 @@ class SerialDecoder:
                 self.states.append(self.readStop2)
     
     def read(self, bit):
+        """Reads one bit based on the current state."""
         self.states[self.state](bit)
         self.state = (self.state + 1) % len(self.states)
     
     def readStart(self, bit):
+        """Checks the start bit."""
         assert bit == 0
         self.bits = []
     
     def readBitLsbToMsb(self, bit):
+        """Reads a most significant bit and puts it in self.bits."""
         self.bits.insert(0, bit)
     
     def readBitMsbToLsb(self, bit):
+        """Reads a least significant bit and puts it in self.bits."""
         self.bits.append(bit)
     
     def readParityEven(self, bit):
+        """Checks that the parity bit is even."""
         assert sum(self.bits) % 2 == bit
     
     def readParityOdd(self, bit):
+        """Checks that the parity bit is odd."""
         assert sum(self.bits) % 2 != bit
     
     def readStop(self, bit):
+        """
+        Checks the first stop bit.
+        Calls self.onByte with the decoded byte.
+        """
         assert bit == 1
         byte = int(''.join([str(bit) for bit in self.bits]), 2)
         self.onByte(byte)
     
     def readStop2(self, bit):
+        """Checks the second stop bit."""
         assert bit == 1
     
     def onByte(self, byte):
+        """Called when a byte has been decoded."""
         pass
--- VcdReader.py
+++ VcdReader.py
@@ -3,11 +3,19 @@ import re
 import sys
 
 class VcdReader:
+    """
+    Reads a VCD file line by line and calls self.onTime and self.onWireValue
+    when either a new time block is read or a wire has a new value.
+    
+    This class should be used as a parent class.
+    Start the reading process by calling the run function.
+    """
     
     def __init__(self, file):
         self.file = file
     
     def run(self):
+        """Starts reading the file line by line."""
         for line in self.file:
             line = line.strip()
             if len(line) > 0:
@@ -15,9 +23,14 @@ class VcdReader:
                     sys.stderr.write("skipping: `%s'\n" % line);
     
     def tryReadLine(self, line):
+        """Tries to interpret one VCD line."""
         return self.tryReadTime(line) or self.tryReadWire(line)
 
     def tryReadTime(self, line):
+        """
+        Tries to interpret a VCD line as a VCD time header.
+        Calls self.onTime if successful.
+        """
         time = re.match('^#(\d+)$', line)
         if not time:
             return False
@@ -25,6 +38,10 @@ class VcdReader:
         return True
     
     def tryReadWire(self, line):
+        """
+        Tries to interpret a VCD line as a wire value change.
+        Calls self.onWireValue if successful.
+        """
         wire = re.match('^(0|1)(\w+)$', line)
         if not wire:
             return False
@@ -34,7 +51,9 @@ class VcdReader:
         return True
     
     def onTime(self, time):
+        """Called when a new timestamp is read."""
         pass
     
     def onWireValue(self, name, value):
+        """Called when a wire value changes."""
         pass
