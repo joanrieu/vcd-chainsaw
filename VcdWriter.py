8acc389cd3caf7e382a56e57b7df604460110b79
Joan Rieu <joan.rieu@gmail.com>
2015-12-04 12:14:38 +0000

    Upgraded VcdWriter to read binary instead of ASCII

--- VcdWriter.py
+++ VcdWriter.py
@@ -1,40 +1,48 @@
-
 import serial
+import sys
 import time
 
-ser = serial.Serial('/dev/ttyACM1', 115200)
-wires = [-1 for i in range(2)]
-file = open("data.vcd", "w")
+# Open serial port
+ser = serial.Serial('/dev/ttyACM0', 1000000)
+
+# Initialize wires and wire count
+wires = [-1 for i in range(8)]
+
+# Open output file (if given)
+if sys.argv > 1:
+    file = open(sys.argv[1], "w")
+else:
+    file = sys.stdout
 
 def wireName(wireId):
+    """Computes a VCD wire name (1 char) from a wire number."""
     return chr(ord("A") + wireId)
 
+# Write the chosen time unit
 file.write("$timescale 1ns $end\n")
 
+# Write the VCD name and description of each wire
 for wireId in range(len(wires)):
     file.write("$var wire 1 %c Digital%d $end\n" % (wireName(wireId), wireId))
 
+# Time reference
 startTime = time.time()
+
+# This variable contains the int describing all the wires in one
 allWires = -1
 
 while True:
 
     # Read the value of all ports as one integer from the serial port
     allWiresOld = allWires
-    allWires = ser.readline().strip()
-    if len(allWires) == 0:
-        continue
-    try:
-        allWires = int(allWires)
-        # Skip this value if all wires are identical
-        if allWires == allWiresOld:
-            continue
-    except ValueError:
-	allWires = -1
+    allWires = ord(ser.read()[0])
+    
+    # Skip this value if all wires are identical
+    if allWires == allWiresOld:
         continue
     
     # Write the header for the new values
-    # TODO Replace delta with value given by the board
+    # TODO Replace this approximate value with a more precise one
     delta = 1000000 * (time.time() - startTime)
     file.write("#%d\n" % (delta))
     
